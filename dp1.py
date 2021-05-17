2f0c74d55562b2ec8d043f4394bc914b7b7e80c1
Nadine278 <78268858+Nadine278@users.noreply.github.com>
2021-05-17 12:51:35 +0300

    Update dp1.py
    Adapter pattern

--- dp1.py
+++ dp1.py
@@ -1,37 +1,49 @@
-from abc import ABCMeta, abstractmethod
+#  Language Translator
 
-class Command(metaclass=ABCMeta):
-    def __init__(self, recv):
-        self.recv = recv
-
-    def execute(self):
-        pass
-
-
-class ConcreteCommand(Command):
-    def __init__(self, recv):
-        self.recv = recv
-
-    def execute(self):
-        self.recv.action()
-
-
-class rcvp:
-    def action(self):
-        print("The person who recieves information")
-
-
-class invp:
-    def command(self, cmd):
-        self.cmd = cmd
-
-    def execute(self):
-        self.cmd.execute()
-
-
-if __name__ == '__main__':
-    recv = rcvp()
-    cmd = ConcreteCommand(recv)
-    inv = invp()
-    inv.command(cmd)
-    inv.execute()
+# Adaptee: Incompatible interface # 1
+class English:
+        def Greeting(self):
+                return "Hello!"
+        def Farewell(self):
+                return "Good byee."
+ 
+# Adapter Class, which takes functionality provided by English, morphs it into functionality expected by the polish person.
+class Translator:
+        
+        _English = None
+        _engphrases = {
+                "Hello!": "CZEŚĆ",
+                "Good byee.": "Do widzenia"
+                }
+                 
+        def __init__(self, English):
+                self._English = English
+         
+# polish speaker: Incompatible interface # 2
+class polish_p:
+        
+        _englToPolish = None
+ 
+        def __init__(self, englToPolish):
+                self._englToPolish = englToPolish
+ 
+        def exchangeGreetings(self):
+                print("Hello!")
+                print( self._englToPolish._engphrases[  self._englToPolish._English.Greeting()  ] )
+ 
+        def exchangeFarewell(self):
+                print("Good bye!")
+                print( self._englToPolish._engphrases[  self._englToPolish._English.Farewell()  ] )
+ 
+# Create an English Speaking person
+English = English()
+ 
+# Create a translator with popular english phrases
+englToPolish = Translator(English)
+ 
+# The polish Person can now get responses in polish
+polish = polish_p(englToPolish)
+ 
+# Two-way conversation in Polish
+polish.exchangeGreetings()
+polish.exchangeFarewell()
